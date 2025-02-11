609ee8f6dd482ca08e3544a16d0dde7a83ee75fb
nickdsullivan <nickdsullivan@gmail.com>
2025-02-11 18:45:31 -0500

    Wrapped up training

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ import torchvision.transforms as transforms
 from diffusers import UNet2DConditionModel, AutoencoderKL
 from dataset import SuperResDataset, get_datasets
 from train import train
-from train2 import train2
+
+from vae_finetuning import train_vae
 from evaluate import evaluate
 import argparse
 
@@ -49,10 +50,6 @@ validation_loader = DataLoader(train_dataset, batch_size=args.batchsize, shuffle
 test_loader = DataLoader(test_dataset, batch_size=args.batchsize, shuffle=False)
 
 
-
-
-
-
 # Model definitions
 # The vae's latent space is input size / 8.  This will cause a bit of trouble later
 # We will need to stretch our 300 x 300 during inference and during embedding.  
@@ -64,23 +61,22 @@ high_res_height = args.high_res_size * .9
 high_res_width = args.high_res_size 
 
 vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse")
+
 if args.checkpoint_vae != None:
     checkpoint = torch.load(args.checkpoint_vae)
     vae.load_state_dict(checkpoint["vae_state_dict"])
 
-# this is the projection for the embedded states
+# This is the projection for the embedded states
 projection = nn.Linear(int(high_res_height//8 * high_res_width//8), 1280).to("mps")
-#projection = nn.Linear(4, 1280).to("mps")
+
 if args.checkpoint_projection != None:
     checkpoint = torch.load(args.checkpoint_projection)
     projection.load_state_dict(checkpoint["projection_state_dict"])
 
 
-
-
 model = UNet2DConditionModel(
-    sample_size=high_res_width,  # Latent space size.  I found this using a script in testing.ipynb
-    in_channels=4,  # Latent space channels (not RGB)
+    sample_size=high_res_width,  # Latent space size.
+    in_channels=4,  
     out_channels=4,
     layers_per_block=2,
     block_out_channels=(128, 256, 512, 512), 
@@ -98,7 +94,7 @@ if args.checkpoint_model != None:
 
 
 
-# Send to my mac device
+# Send to my mac device  This will have be changed
 vae.to("mps")
 projection.to("mps")
 model.to("mps")
@@ -107,10 +103,9 @@ model.to("mps")
 
 model.train()
 projection.train()
-#vae.train()
 print("Models and data loaded. Starting Training.")
 if args.train:
-    train2(train_loader,
+    train(train_loader,
         vae = vae, projection = projection, model = model,
         num_train_steps=len(train_dataset), 
         lr = 1e-4, weight_decay=1e-7,
@@ -120,7 +115,7 @@ if args.train:
         low_res_height = int(low_res_height),
         low_res_width = int(low_res_width),
         batch_size=args.batchsize,
-        num_epoch=10)
+        num_epoch=1)
 if args.eval:
     print(evaluate(validation_loader,
             vae = vae, projection = projection, model = model,
--- src/train.py
+++ src/train.py
@@ -12,10 +12,8 @@ from PIL import Image
 from evaluate import save_image
 
 from diffusers import UNet2DConditionModel, AutoencoderKL, DDPMScheduler
+import torch.nn.functional as F
 
-from dataset import SuperResDataset, get_datasets
-
-import time
 
 def train(train_loader, vae, projection, model,
           high_res_height, high_res_width,
@@ -26,70 +24,76 @@ def train(train_loader, vae, projection, model,
           checkpoint_frequency = 10,
           batch_size = 2,
           num_epoch = 1,
-
           ):
     
     high_res_size = high_res_width
     low_res_size  = low_res_width
     # Loss and noise scheduling
     scheduler = DDPMScheduler(
-        num_train_timesteps=num_train_steps,  # Total number of timesteps
-        beta_start=0.00001,         # Starting noise level
-        beta_end=0.02,             # Ending noise level
-        beta_schedule="linear"     # Noise schedule type
+        num_train_timesteps=int((num_train_steps/batch_size)*num_epoch),  
+        beta_start=0.0001,         
+        beta_end=0.02,             
+        beta_schedule="linear"     
     )
-
-
-
+   
+    # Loss functions and optimizer
     loss_fn = nn.MSELoss()
     optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
     iterations = 0
-    start_time = time.time()
-    cum_loss = 0
-    cum_timestep = 0
+    # Learning rate schedule
+    lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.1, total_iters=num_epoch*(num_train_steps/batch_size))
+    
+    # The initial checkpoint
+    checkpoint = {
+        "epoch": iterations + 1,
+        "model_state_dict": model.state_dict(),
+        "vae_state_dict": vae.state_dict(),
+        "projection_state_dict": projection.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "loss": 0,
+    }
+    
+
+    losses = []
     for epoch in range(num_epoch):
         for low_res, high_res in train_loader:
             low_res, high_res = low_res.to("mps"), high_res.to("mps")
 
             # Encode high-res image to latent space
-            z_high = vae.encode(high_res).latent_dist.sample()
+            with torch.no_grad():
+                z_high = vae.encode(high_res).latent_dist.sample()
 
-            # Add noise to latent representation
-            noise = torch.randn_like(z_high)
-            max_noise =  min(max(int((1000*(iterations+1))/(num_train_steps/batch_size)),200),1000)
-            timesteps = torch.randint(0, max_noise, (low_res.shape[0],), device="mps")
-            noisy_z = scheduler.add_noise(z_high, noise, timesteps.long())
+                # Add noise to latent representation
+                noise = torch.randn_like(z_high)
+                max_noise =  min(max(int((100*(iterations+1))/((num_train_steps/batch_size)*num_epoch)),1),1000)
+                timesteps = torch.randint(0, max_noise, (batch_size,), device="mps").long()
+                noisy_z = scheduler.add_noise(z_high, noise, timesteps)
             
 
             # Resize low_res to match VAE's 600x600
-            low_res_resized = torch.nn.functional.interpolate(low_res, size=(high_res_height, high_res_width), mode='bilinear')
-            # # Encode low_res into a latent tensor using VAE
+            low_res_resized = torch.nn.functional.interpolate(low_res, size=(high_res_height,high_res_width), mode='bilinear')
+            # Encode low_res into a latent tensor using VAE
+            # We won't train the VAE so we use no grad
             with torch.no_grad():
                 encoded_low_res = vae.encode(low_res_resized).latent_dist.sample() 
-            
-            batch_size, channels, h, w = encoded_low_res.shape
-            encoded_low_res = encoded_low_res.permute(0, 3, 2, 1).reshape(batch_size, h * w, channels)
+                batch_size, channels, h, w = encoded_low_res.shape
+                encoded_low_res = encoded_low_res.permute(0, 3, 2, 1).reshape(batch_size, channels, h * w)
             encoded_low_res = projection(encoded_low_res).to("mps")
+
+            # Model prediction
             predicted_noise = model(noisy_z, timesteps, encoder_hidden_states=encoded_low_res).sample
-            loss = loss_fn(predicted_noise, noise)
-            loss.backward()
-
-            
+            # Back prop
+            loss = loss_fn(noisy_z, predicted_noise)
             optimizer.zero_grad()
+            loss.backward()
             optimizer.step()
-
-            print(f"Iteration [{iterations+1}/{int(num_train_steps/batch_size)}] | Loss: {loss.item():.4f} | Timestep {timesteps[0]:.0f}")    
-
-
+            lr_scheduler.step()
+            losses.append(loss.item())
+            print(f"Iteration [{iterations+1}/{(int(num_train_steps/batch_size)*num_epoch)}] | Loss: {sum(losses[len(losses)-10:])/10:.4f} | Timestep {timesteps[0].item():.0f}")    
+            
             if iterations % checkpoint_frequency == 0:
-               
-               
-                # cum_loss = cum_loss + loss.item()
-                # cum_timestep = cum_timestep + torch.mean(timesteps.float())
-                # print(f"Iteration [{iterations+1}/{int(num_train_steps/batch_size)}] | Loss: {cum_loss:.4f} | Timestep {cum_timestep:.0f}")    
-                # cum_loss = 0
-                # cum_timestep = 0
-                
+                # Save the model and checkpoint
+                torch.save(checkpoint,  f"{save_dir}/checkpoints/{low_res_size}_{high_res_size}/backup.pt")
                 checkpoint = {
                     "epoch": iterations + 1,
                     "model_state_dict": model.state_dict(),
@@ -99,40 +103,39 @@ def train(train_loader, vae, projection, model,
                     "loss": loss.item(),
                 }
                 os.makedirs( f"{save_dir}/checkpoints/{low_res_size}_{high_res_size}", exist_ok=True)
-                #torch.save(checkpoint,  f"{save_dir}/checkpoints/{low_res_size}_{high_res_size}/checkpoint_epoch_{iterations+1}.pt")
-                torch.save(checkpoint,  f"{save_dir}/checkpoints/{low_res_size}_{high_res_size}/latest4.pt")
+                torch.save(checkpoint,  f"{save_dir}/checkpoints/{low_res_size}_{high_res_size}/latest.pt")
+                # Denoise the images to save
                 with torch.no_grad():
-                    #Save a complete image for visualization
-                    denoised_z = noisy_z - predicted_noise
+                    denoised_z_batch = []
+                    for i in range(noisy_z.shape[0]):  
+                        t = timesteps[i].item() 
+                        denoised_output = scheduler.step(
+                            predicted_noise[i].unsqueeze(0),  
+                            t, 
+                            noisy_z[i].unsqueeze(0) 
+                        )
+                        denoised_z_batch.append(denoised_output.prev_sample)
+                    denoised_z = torch.cat(denoised_z_batch, dim=0)
                     high_res_predicted = vae.decode(denoised_z).sample.clamp(0, 1)
+                    og_image_noisy = vae.decode(noisy_z).sample.clamp(0,1) # This image makes sure the unet is doing something
+                    non_noisy = vae.decode(z_high).sample.clamp(0, 1)
                 
                     image_dir = f"visual_verification/{low_res_size}_{high_res_size}/{iterations}"
                     os.makedirs(f"{image_dir}", exist_ok=True)
-                    high_res_predicted = high_res_predicted[0]
-                    high_res = high_res[0]
-                    low_res = low_res[0]
-                    #save_image(noisy_z[0], f"{image_dir}/noisey_z_{timesteps}")
-                    #save_image(predicted_noise[0], f"{image_dir}/predicted_noise{timesteps[0].item()}")
-                    save_image(high_res_predicted, f"{image_dir}/predicted_{timesteps[0].item()}")
-                    save_image(high_res, f"{image_dir}/high_res")
-                    save_image(low_res, f"{image_dir}/low_res")
+                    save_image(og_image_noisy[0], f"{image_dir}/og_image_noisy")
+                    save_image(high_res_predicted[0], f"{image_dir}/predicted")
+                    save_image(high_res[0], f"{image_dir}/high_res")
+                    save_image(low_res[0], f"{image_dir}/low_res")
+                    save_image(non_noisy[0], f"{image_dir}/non_noisy")
                     
-            
-            #del low_res, encoded_low_res, high_res, z_high, noise, noisy_z, predicted_noise
-            #del low_res, high_res, z_high, noise, noisy_z, predicted_noise
-            #torch.mps.empty_cache()
-            
+            # Manually delete things we don't need
+            del low_res, encoded_low_res, high_res, z_high, noise, noisy_z, predicted_noise
+            torch.mps.empty_cache()
             iterations = iterations + 1
-    checkpoint = {
-        "epoch": iterations + 1,
-        "model_state_dict": model.state_dict(),
-        "optimizer_state_dict": optimizer.state_dict(),
-        "loss": loss.item(),
-    }
+
+    # Save the final models
     os.makedirs( f"{save_dir}/final/{low_res_size}_{high_res_size}", exist_ok=True)
-
-    torch.save(model.state_dict(),      os.path.join(save_dir+ "/final/", f"{low_res_size}_{high_res_size}/final_model.pt"))
-    torch.save(vae.state_dict(),        os.path.join(save_dir + "/final/", f"{low_res_size}_{high_res_size}/final_vae.pt"))
-    torch.save(projection.state_dict(), os.path.join(save_dir+"/final/", f"{low_res_size}_{high_res_size}/final_projection.pt"))
-
+    torch.save(model.state_dict(),      os.path.join(save_dir+ "/final/", f"{low_res_size}_{high_res_size}/final_model2.pt"))
+    torch.save(vae.state_dict(),        os.path.join(save_dir + "/final/", f"{low_res_size}_{high_res_size}/final_vae2.pt"))
+    torch.save(projection.state_dict(), os.path.join(save_dir+"/final/", f"{low_res_size}_{high_res_size}/final_projection2.pt"))
     print("Final models saved.")
